d1fd5646e9027af33d46e0941d673de28bd582bf
Thushara_thilan <142692846+LUC1f3R-0@users.noreply.github.com>
2025-08-15 12:45:11 +0530

    temporary commit before switching to main

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,10 @@
 import { Controller, Get } from '@nestjs/common';
 import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { AppService } from './app.service';
-
 @ApiTags('app')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
   @Get()
   @ApiOperation({
     summary: 'Get hello message',
